20cad7946af67eeed39911c1aadc634ff8d75f6f
Genny Centeno <gennycm13@gmail.com>
2019-07-15 15:27:46 -0300

    Changes

--- a6_backend/app.js
+++ a6_backend/app.js
@@ -1,33 +1,14 @@
 var express = require('express');
 var app = express();
-const mysql = require('mysql');
 
-const port = 8080;
-
-const db = mysql.createConnection({
-    host: "cc-employer-db.mysql.database.azure.com",
-    user: "cc-team6@cc-employer-db",
-    password: "cloudgladiators_6",
-    port: 3306,
-    database: "assignment6"
+app.get('/', function (req, res) {
+    res.sendStatus(200);
 });
 
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
+app.get('/employees', function (req, res) {
+    res.json('{employees: [{"createdAt":1562961607326,"updatedAt":1562961607326,"id":1,"name":"Genny","lastName":"Centeno","address":"6050 University Road","age":26,"phoneNumber":"9852137458"},{"createdAt":1562961610909,"updatedAt":1562961610909,"id":2,"name":"Neharika","lastName":"Sehgal","address":"6050 University Road","age":31,"phoneNumber":"9022137458"},{"createdAt":null,"updatedAt":null,"id":8,"name":"Jose","lastName":"Ancona","address":"1333 University Road","age":25,"phoneNumber":"7892137789"}]}');
 });
-global.db = db;
 
-const {getEmployees, createEmployee,sendSuccessStatus} = require('./routes/index');
-
-// routes for the app
-app.get('/', sendSuccessStatus);
-app.get('/api/employees', getEmployees);
-app.get('/api/employee/create', createEmployee);
-
-app.listen(port, function () {
-    console.log('Example app listening on port '+ port);
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });

b193cd2a78a57cd931fd274f87a5ba57e701512e
Genny Centeno <gennycm13@gmail.com>
2019-07-15 18:13:46 -0300

    Added service to sum numbers

--- a6_backend/app.js
+++ a6_backend/app.js
@@ -2,18 +2,28 @@ var express = require('express');
 var app = express();
 
 
-
 app.get('/_ah/health', (req, res) => {
     res.status(200).send('ok');
 });
 
 app.get('/', function (req, res) {
-    res.sendStatus(200);
+    res.status(200);
 });
 
 app.get('/employees', function (req, res) {
     res.json("{employees: [{'createdAt':1562961607326,'updatedAt':1562961607326,'id':1,'name':'Genny','lastName':'Centeno','address':'6050 University Road','age':26,'phoneNumber':'9852137458'},{'createdAt':1562961610909,'updatedAt':1562961610909,'id':2,'name':'Neharika','lastName':'Sehgal','address':'6050 University Road','age':31,'phoneNumber':'9022137458'},{'createdAt':null,'updatedAt':null,'id':8,'name':'Jose','lastName':'Ancona','address':'1333 University Road','age':25,'phoneNumber':'7892137789'}]}");
 });
+
+app.get('/sum/:maxNum', function (req, res) {
+    let maxNum = req.params.maxNum;
+    let total = 0;
+
+    for (let i = 1; i <= maxNum; i++) {
+        total += i;
+    }
+    res.json({'total': total});
+});
+
 
 // Basic 404 handler
 app.use((req, res) => {

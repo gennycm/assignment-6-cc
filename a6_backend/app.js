b6ed18bb46821b6fb7d71a0eea29d48f704f2732
Genny Centeno <gennycm13@gmail.com>
2019-07-15 15:16:22 -0300

    changed app.js and startup script

--- a6_backend/app.js
+++ a6_backend/app.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 const mysql = require('mysql');
-const app = express();
 
-const {getEmployees, createEmployee,sendSuccessStatus} = require('./routes/index');
+const port = 8080;
 
-const port = 80;
-
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection({
     host: "cc-employer-db.mysql.database.azure.com",
     user: "cc-team6@cc-employer-db",
@@ -26,25 +21,13 @@ db.connect((err) => {
 });
 global.db = db;
 
-// configure middleware
-app.set('port', process.env.port || port); // set express to use this port
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); // parse form data client
+const {getEmployees, createEmployee,sendSuccessStatus} = require('./routes/index');
 
 // routes for the app
 app.get('/', sendSuccessStatus);
 app.get('/api/employees', getEmployees);
 app.get('/api/employee/create', createEmployee);
 
-
-// set the app to listen on the port
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-}).on('error', function(err){
-    console.log('on error handler');
-    console.log(err);
+app.listen(port, function () {
+    console.log('Example app listening on port '+ port);
 });
-
-
-
-module.exports = app;

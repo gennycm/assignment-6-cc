375d8bad4be183144cd4ea80990b0a57aedb7d8c
Genny Centeno <gennycm13@gmail.com>
2019-07-15 15:29:08 -0300

    Changes

--- a6_backend/app.js
+++ a6_backend/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/employees', function (req, res) {
-    res.json('{employees: [{"createdAt":1562961607326,"updatedAt":1562961607326,"id":1,"name":"Genny","lastName":"Centeno","address":"6050 University Road","age":26,"phoneNumber":"9852137458"},{"createdAt":1562961610909,"updatedAt":1562961610909,"id":2,"name":"Neharika","lastName":"Sehgal","address":"6050 University Road","age":31,"phoneNumber":"9022137458"},{"createdAt":null,"updatedAt":null,"id":8,"name":"Jose","lastName":"Ancona","address":"1333 University Road","age":25,"phoneNumber":"7892137789"}]}');
+    res.json("{employees: [{'createdAt':1562961607326,'updatedAt':1562961607326,'id':1,'name':'Genny','lastName':'Centeno','address':'6050 University Road','age':26,'phoneNumber':'9852137458'},{'createdAt':1562961610909,'updatedAt':1562961610909,'id':2,'name':'Neharika','lastName':'Sehgal','address':'6050 University Road','age':31,'phoneNumber':'9022137458'},{'createdAt':null,'updatedAt':null,'id':8,'name':'Jose','lastName':'Ancona','address':'1333 University Road','age':25,'phoneNumber':'7892137789'}]}");
 });
 
 app.listen(8080, function () {

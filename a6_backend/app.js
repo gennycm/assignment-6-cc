7989edb4ad3d0c4822bfe5679c556e960dc7d7ae
Genny Centeno <gennycm13@gmail.com>
2019-07-17 11:48:25 -0300

    Changed endpoint

--- a6_backend/app.js
+++ a6_backend/app.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
 
-
-app.get('/_ah/health', (req, res) => {
-    res.status(200).send('ok');
+app.get('/employees', function (req, res) {
+    res.json("{employees: [{'createdAt':1562961607326,'updatedAt':1562961607326,'id':1,'name':'Genny','lastName':'Centeno','address':'6050 University Road','age':26,'phoneNumber':'9852137458'},{'createdAt':1562961610909,'updatedAt':1562961610909,'id':2,'name':'Neharika','lastName':'Sehgal','address':'6050 University Road','age':31,'phoneNumber':'9022137458'},{'createdAt':null,'updatedAt':null,'id':8,'name':'Jose','lastName':'Ancona','address':'1333 University Road','age':25,'phoneNumber':'7892137789'}]}");
 });
+
 
 app.get('/', function (req, res) {
     res.status(200);
 });
 
-app.get('/employees', function (req, res) {
-    res.json("{employees: [{'createdAt':1562961607326,'updatedAt':1562961607326,'id':1,'name':'Genny','lastName':'Centeno','address':'6050 University Road','age':26,'phoneNumber':'9852137458'},{'createdAt':1562961610909,'updatedAt':1562961610909,'id':2,'name':'Neharika','lastName':'Sehgal','address':'6050 University Road','age':31,'phoneNumber':'9022137458'},{'createdAt':null,'updatedAt':null,'id':8,'name':'Jose','lastName':'Ancona','address':'1333 University Road','age':25,'phoneNumber':'7892137789'}]}");
+app.get('/_ah/health', (req, res) => {
+    res.status(200).send('ok');
 });
 
-app.get('/sum/:maxNum', function (req, res) {
+app.get('/sum', function (req, res) {
 
     //Source: https://tecadmin.net/get-current-date-time-javascript/
     let TSwsStart = new Date();
